7fb8280a72eead74080629cb834643ed7ec4816a
Juan Morales <juan.morales@picsolve.com>
2015-08-30 14:58:56 +0100

    added settings as example; modified return values of functions with sideeffects

--- camgrab.py
+++ camgrab.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 
-TIME_WINDOW=15 # seconds to keep the window open
-PICTURE_INTERVAL=25 # take a picture every this seconds
+import settings 
+
+print dir(settings)
+#TIME_WINDOW=settings.TIME_WINDOW if settings.TIME_WINDOW is not None else 15 # seconds to keep the window open
+TIME_WINDOW=15
+#PICTURE_INTERVAL=settings.PICTURE_INTERVAL or 25 # take a image every this seconds
+PICTURE_INTERVAL=25
+CAMERA=settings.CAMERA or 0 # the id of the camera for openCV (maybe /dev/video<number> )
+SAVE_FOLDER=settings.SAVE_FOLDER or "." # folder to put pictures and barcodes
 
 
 
@@ -16,60 +23,91 @@ PICTURE_INTERVAL=25 # take a picture every this seconds
 # something like libgphoto2.
 
 
-from time import sleep
+import time
 from cv2 import *
 
 
 
 def grab_pic(camera_index=0):
     """
-    take a picture from the specified camera
+    take a image from the specified camera
     camera_index :: default = 0; just the number of the cam on the system (is compatible with many)
     """
-    try:
-        cam = VideoCapture(camera_index)
-        s, img = cam.read()
-        if s: # the frame was captured without errors
-            return img
-        else:
-            raise IOError("Sorry, couldn't get the picture")
-    except IOError as e:
-        print("ERROR",e)
-        return None
-    #return img # TODO:Confirm that img is None upon failure to be able to enable this code
+    cam = VideoCapture(camera_index)
+    s, img = cam.read()
+    if s: # the frame was captured without errors
+        msg =  "DEBUG: Picture retrieved without errors"
+        print msg
+        return img, msg
+    else:
+        msg = "ERROR: Couldn't retrieve image from camera {0}".format(camera_index)
+        print(msg)
+        return None, msg
+
+    #return img # TODO:Confirm that image is None upon failure to be able to enable this code
 
 
 
-def display_picture(picture = None):
+def display_image(img = None,img_id=None):
     """
-    just create a simple window to display the picture. 
+    just create a simple window to display the image. 
     The window should go away after a few seconds
     TODO: for now I have implemented the keypress to close the window
     """
-    assert picture is not None # as it means that no pic has been passed
-    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
-    imshow("cam-test",picture)
-    waitKey(TIME_WINDOW)
+    assert img is not None # as it means that no pic has been passed
+    assert img_id is not None # as it means that no pic has been passed
+    windowName = "{0}".format(img_id)
+    namedWindow(windowName,CV_WINDOW_AUTOSIZE)
+    imshow(windowName,img)
+    print "DEBUG:", "TIME_WINDOWN type: ", type(TIME_WINDOW)
+        # TODO: for some extrange reason, using an int variable doesnt work. 
+        # python passes by object reference (object references are passed by value)
+        # so it should work.
+    waitKey(300)
     destroyWindow("cam-test")
+    return img, "INFO: image {0} displayed".format(img_id)
 
 
-def save_picture(picture=None):
+def save_image(img=None,img_id=None):
     """
-    save the picture to disk
+    save the image to disk
+    img :: the picture blob
+    img_id :: a pregenerated id for this image
     """
-    imwrite("filename.jpg",picture) #save image
-    print("INFO","Picture saved")
+    assert img is not None
+    assert img_id is not None
+    try:
+        imwrite(r"{0}\{1}.jpg".format(SAVE_FOLDER,img_id),img) #save image
+        print("INFO","Picture {0} saved".format(img_id))
+        msg = "INFO \t saved image {0}".format(img_id)
+    except Exception as e:
+        #TODO: Pokemon exception. Please remove
+        msg = "ERROR \t Unable to save image {0},{1}".format(
+            img_id,
+            e)
+        print("ERROR", msg)
+    return img,msg 
+
+
+def get_img_id(timestamp, camera):
+    """
+    create an id for the image. Uses timestamp and camera of origin.
+    timestamp :: seconds since epoch (in UTC usually)
+    camera :: a string to identify the camera of origin
+    """
+    timeString = time.strftime("%Y%m%d_%H%M_%S", timestamp)
+    return "{0}_cam{1}".format(timeString,camera)
 
 
 if __name__=="__main__":
     """
     """
-    # we start grabbing pictures every 10 seconds or so
+    # we start grabbing image every 10 seconds or so
 
-    while True:
-        print("INFO","Taking pictures and stuff")
-        camera = 0 # the index for the camera. It should be taken from settings, or a default which is 0 often
-        picture = grab_pic(camera) # get the actual picture from the camera
-        display_picture(picture) # open a window with it
-        save_picture(picture) # put it in a file
-        sleep(PICTURE_INTERVAL)
+    for x in range(2):
+        print("INFO","Taking image and stuff")
+        img,msg = grab_pic(CAMERA) # get the actual image from the camera
+        img_id = get_img_id(time.gmtime(), CAMERA)  # get an id to track the picture
+        display_image(img, img_id) # open a window with it
+        save_image(img, img_id) # put it in a file
+        time.sleep(PICTURE_INTERVAL)
---
+++ settings.py
@@ -0,0 +1,15 @@
+
+"""
+This is the settings file.
+Here you should have the bare minimum needed to run the software, 
+but if you miss a setting it will just use the default.
+
+The commented out settings are here just for reference.
+
+TODO: either delete or use proper explanations
+"""
+
+#TIME_WINDOW=15 # seconds to keep the window open
+#PICTURE_INTERVAL=25 # take a image every this seconds
+CAMERA=0 # the id of the camera for openCV (maybe /dev/video<number> )
+SAVE_FOLDER="captures" # folder to put pictures and barcodes. default to current dir.
